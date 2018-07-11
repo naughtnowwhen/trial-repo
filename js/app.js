11a6078651db8990f9a7d58cbd8a05366589d8a2
Peter Murphy <naughtnowwhen@gmail.com>
2018-07-10 23:41:26 -0700

    changed my questions and the text in my p tags

--- js/app.js
+++ js/app.js
@@ -1,61 +1,61 @@
     'use strict';
 
         prompt("please answer the questions with either a 'Y' an 'N', thanks.");
-        var qA = prompt("are you hungry?");
+        var qA = prompt("Am I tired?");
 
         if (qA.toUpperCase() === 'Y') {
-            alert("go and eat");
+            alert("You guessed it, luckily i've got some coffee");
         }   
         else if (qA.toUpperCase() === 'N') {
-            alert("don't eat");
+            alert("No? That was some good coffee!");
         }
         else {
             alert("invalid response");
         }
 
-        var qB = prompt("are you tired");
+        var qB = prompt("am I ginger?");
 
         if (qB.toUpperCase() === 'Y' || (qB.toUpperCase() === 'YES')) {
-            alert("go and sleep");
+            alert("Yes, ginger-ish");
         }   
         else if (qB.toUpperCase() === 'N' || (qB.toUpperCase() === 'NO')) {
-            alert("don't sleep");
+            alert("No? ok, strawberry blond, whatever... but if i'm not ginger I at least like it with my sushi");
         }
         else {
             alert("invalid response");
         }
 
-        var qC = prompt("are you angry?");
+        var qC = prompt("am I excited?");
 
         if (qC.toUpperCase() === 'Y' || (qC.toUpperCase() === 'Yes')){
-            alert("go and rage");
+            alert("Yes, this stuff is exciting!");
         }   
         else if (qC.toUpperCase() === 'N' || (qC.toUpperCase() === 'NO')) {
-            alert("keep calm");
+            alert("No? Hmm, excitement ebbs and flows doesn't it? Maybe time for more coffee?");
         }
         else {
             alert("invalid response");
         }
 
-        var qD = prompt("are you mad as hell and not going to take it anymore?");
+        var qD = prompt("Am I daunted?");
 
         if (qD.toUpperCase() === 'Y' || (qD.toUpperCase() === 'Yes')){
-            alert("Shout out your window");
+            alert("Well, sure, Brandy wasn't kidding about the whole 'drinking from a firehose' thing was she?");
         }   
         else if (qD.toUpperCase() === 'N' || (qD.toUpperCase() === 'NO')) {
-            alert("continue taking it");
+            alert("you're too kind, I hope to share your opinion someday");
         }
         else {
             alert("invalid response");
         }
 
-        var qE = prompt("are you experienced?");
+        var qE = prompt("am I daydreaming?");
 
         if (qE.toUpperCase() === 'Y' || (qE.toUpperCase() === 'Yes')){
-            alert("Use you experience to gain even more");
+            alert("Yeah, funny how that goes, the mind sure does wander huh?");
         }   
         else if (qE.toUpperCase() === 'N' || (qE.toUpperCase() === 'NO')) {
-            alert("Get some experience");
+            alert("No? where'd all this great focus come from? More please!");
         }
         else {
             alert("invalid response");
