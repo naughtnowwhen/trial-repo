4964b79c28675f3db418d8a76f7c52ecbb621ccd
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-07-12 14:12:53 -0700

    Wrap first 5 questions in a function

--- js/app.js
+++ js/app.js
@@ -1,73 +1,76 @@
-    'use strict';
+'use strict';
 
-        prompt("please answer the questions with either a 'Y' an 'N', thanks.");
-        var qA = prompt("Am I tired?");
+// function firstQuestions() {
+//     alert("please answer the questions with either a 'Y' an 'N', thanks.");
+//     var qA = prompt("Am I tired?");
 
-        if (qA.toUpperCase() === 'Y') {
-            alert("You guessed it, luckily i've got some coffee");
-        }   
-        else if (qA.toUpperCase() === 'N') {
-            alert("No? That was some good coffee!");
-        }
-        else {
-            alert("invalid response");
-        }
+//     if (qA.toUpperCase() === 'Y') {
+//         alert("You guessed it, luckily i've got some coffee");
+//     }
+//     else if (qA.toUpperCase() === 'N') {
+//         alert("No? That was some good coffee!");
+//     }
+//     else {
+//         alert("invalid response");
+//     }
 
-        var qB = prompt("am I ginger?");
+//     var qB = prompt("am I ginger?");
 
-        if (qB.toUpperCase() === 'Y' || (qB.toUpperCase() === 'YES')) {
-            alert("Yes, ginger-ish");
-        }   
-        else if (qB.toUpperCase() === 'N' || (qB.toUpperCase() === 'NO')) {
-            alert("No? ok, strawberry blond, whatever... but if i'm not ginger I at least like it with my sushi");
-        }
-        else {
-            alert("invalid response");
-        }
+//     if (qB.toUpperCase() === 'Y' || (qB.toUpperCase() === 'YES')) {
+//         alert("Yes, ginger-ish");
+//     }
+//     else if (qB.toUpperCase() === 'N' || (qB.toUpperCase() === 'NO')) {
+//         alert("No? ok, strawberry blond, whatever... but if i'm not ginger I at least like it with my sushi");
+//     }
+//     else {
+//         alert("invalid response");
+//     }
 
-        var qC = prompt("am I excited?");
+//     var qC = prompt("am I excited?");
 
-        if (qC.toUpperCase() === 'Y' || (qC.toUpperCase() === 'Yes')){
-            alert("Yes, this stuff is exciting!");
-        }   
-        else if (qC.toUpperCase() === 'N' || (qC.toUpperCase() === 'NO')) {
-            alert("No? Hmm, excitement ebbs and flows doesn't it? Maybe time for more coffee?");
-        }
-        else {
-            alert("invalid response");
-        }
+//     if (qC.toUpperCase() === 'Y' || (qC.toUpperCase() === 'Yes')){
+//         alert("Yes, this stuff is exciting!");
+//     }
+//     else if (qC.toUpperCase() === 'N' || (qC.toUpperCase() === 'NO')) {
+//         alert("No? Hmm, excitement ebbs and flows doesn't it? Maybe time for more coffee?");
+//     }
+//     else {
+//         alert("invalid response");
+//     }
 
-        var qD = prompt("Am I daunted?");
+//     var qD = prompt("Am I daunted?");
 
-        if (qD.toUpperCase() === 'Y' || (qD.toUpperCase() === 'Yes')){
-            alert("Well, sure, Brandy wasn't kidding about the whole 'drinking from a firehose' thing was she?");
-        }   
-        else if (qD.toUpperCase() === 'N' || (qD.toUpperCase() === 'NO')) {
-            alert("you're too kind, I hope to share your opinion someday");
-        }
-        else {
-            alert("invalid response");
-        }
+//     if (qD.toUpperCase() === 'Y' || (qD.toUpperCase() === 'Yes')){
+//         alert("Well, sure, Brandy wasn't kidding about the whole 'drinking from a firehose' thing was she?");
+//     }
+//     else if (qD.toUpperCase() === 'N' || (qD.toUpperCase() === 'NO')) {
+//         alert("you're too kind, I hope to share your opinion someday");
+//     }
+//     else {
+//         alert("invalid response");
+//     }
 
-        var qE = prompt("am I daydreaming?");
+//     var qE = prompt("am I daydreaming?");
 
-        if (qE.toUpperCase() === 'Y' || (qE.toUpperCase() === 'Yes')){
-            alert("Yeah, funny how that goes, the mind sure does wander huh?");
-        }   
-        else if (qE.toUpperCase() === 'N' || (qE.toUpperCase() === 'NO')) {
-            alert("No? where'd all this great focus come from? More please!");
-        }
-        else {
-            alert("invalid response");
-        }
+//     if (qE.toUpperCase() === 'Y' || (qE.toUpperCase() === 'Yes')){
+//         alert("Yeah, funny how that goes, the mind sure does wander huh?");
+//     }
+//     else if (qE.toUpperCase() === 'N' || (qE.toUpperCase() === 'NO')) {
+//         alert("No? where'd all this great focus come from? More please!");
+//     }
+//     else {
+//         alert("invalid response");
+//     }
 
-        console.log(qA + " = " + 'qA');
-        console.log(qB + " = " + 'qB');
-        console.log(qC + " = " +'qC');
-        console.log(qD + " = " + 'qD');
-        console.log(qE + " = " + 'qE');
+//     console.log(qA + " = " + 'qA');
+//     console.log(qB + " = " + 'qB');
+//     console.log(qC + " = " +'qC');
+//     console.log(qD + " = " + 'qD');
+//     console.log(qE + " = " + 'qE');
+// }
+// firstQuestions();
 
-       
+
 
 
 /// beginning of Array checker
@@ -105,7 +108,7 @@ while (guessesLeft > 0)
   else {
     score++;
     guessesLeft --;
-    // hmm, i don't know how to implement this quite, but I know that I want something like this to subtract the user guess from the myArray so that they can't keep guessing an already known value and running up their score. 
+    // hmm, i don't know how to implement this quite, but I know that I want something like this to subtract the user guess from the myArray so that they can't keep guessing an already known value and running up their score.
     // myArray.slice(userGuess);
     alert('thats right! you have ' + guessesLeft + ' left and your score is currently ' + score);
 
@@ -117,6 +120,6 @@ if (guessesLeft === 0) {
     alert("you're done! Your final score was" + score);
 }
 
-       
+
 
 
